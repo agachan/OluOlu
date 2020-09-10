31f6cc6ca53860084bcb37f2d1328f32a5fb22ac
Agachan <agachan0930@gmail.com>
2020-09-10 14:12:39 +0900

    Initial Commit

---
+++ OluOlu/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OluOlu
+//
+//  Created by AGA TOMOHIRO on 2020/09/10.
+//  Copyright © 2020 AGA TOMOHIRO. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
